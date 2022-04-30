520bc376470b3ab18b7df5fcc51c047e87dc7c36
DRodrigues17 <adanielluiz71@gmail.com>
2022-04-29 22:16:46 -0300

    0.2.2

---
+++ 0.2.2.js
@@ -0,0 +1,35 @@
+const usuario={
+  username: "drodrigues",
+  email: "generic@email.com",
+  senha: "********",
+  artigoss:{
+    finalizados:[],
+    incompletos:[]
+  },
+
+}
+
+1= {
+  id: 1,
+  titulo: "buracos negros e materia escura",
+  genero: "pesquisa",
+  topicos: ["ciencia", "ficcao cinetifica"],
+  finalizado: true,
+  criadores : [usuario]
+},
+2= {
+  id: 2,
+  titulo: "chernobyl, o impacto",
+  genero: "documentario",
+  topicos: ["documentario", "estudo social"],
+  finalizada: false,
+  criadores : [usuario]
+},
+3= {
+  id: 3,
+  titulo: "godzilla",
+  genero: "critica",
+  topicos: ["opniao", "entretenimento"],
+  finalizada: true,
+  criadores : [usuario]
+}
