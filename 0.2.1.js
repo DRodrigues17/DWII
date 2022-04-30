86d92ea0235b3ba0fb055e1b644f5e1d0d45550a
DRodrigues17 <adanielluiz71@gmail.com>
2022-04-29 21:40:31 -0300

    1 file added

---
+++ 0.2.1.js
@@ -0,0 +1,21 @@
+const filmes = [
+  1,2,3
+]
+1= {
+  titulo: "Django Livre",
+  diretor: "quentin tarantino",
+  estilo: "aventura",
+  atores : ["Samuel L. Jackson"," Jamie Foxx","Leonaro Dicaprio"]
+},
+2= {
+  titulo: "SnyderCut",
+  diretor:"zack snyder",
+  estilo:"aventura",
+  atores : ["ben afleck", "herny cavil", "gal gadot"]
+},
+3= {
+  titulo: "godzilla",
+  diretor: "adam wingard",
+  estilo: "aventura",
+  atores : ["Ken Watanabe","Bryan Cranston","Aaron Johnson"]
+}
